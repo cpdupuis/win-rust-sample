0e2768ecd2bb768b415d8f499cb9505e1463734b
Chris DuPuis <cpdupuis@gmail.com>
2024-11-24 08:25:14 -0800

    Get something working
    Using code from https://learn.microsoft.com/en-us/windows/dev-environment/rust/rss-reader-rust-for-windows

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+
+use windows::{
+    core::*,
+    Win32::UI::WindowsAndMessaging::*
+};
+
 fn main() {
     println!("Hello, world!");
+    unsafe {
+        MessageBoxA(None, s!("Ansi"), s!("World"), MB_OK);
+        MessageBoxW(None, w!("Wide"), w!("World"), MB_OK);
+    }
 }
