750b9354b54bd4d8f15aa2591b7878dd13314cf0
Chris DuPuis <cpdupuis@gmail.com>
2024-11-24 11:30:55 -0800

    Now it's an appliacation that shows and window and exits when the window is closed

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,90 @@
-
 use windows::{
     core::*,
-    Win32::UI::WindowsAndMessaging::*
+    Win32::Graphics::Gdi::*,
+    Win32::{Foundation::*, System::LibraryLoader::GetModuleHandleW, UI::WindowsAndMessaging::*},
 };
 
+
 fn main() {
-    println!("Hello, world!");
     unsafe {
-        MessageBoxA(None, s!("Ansi"), s!("World"), MB_OK);
-        MessageBoxW(None, w!("Wide"), w!("World"), MB_OK);
+        let hwnd = create_main_window().expect("Expected a window");
+        let _ = ShowWindow(hwnd, SW_SHOWNORMAL);
+        main_loop();
+    }
+}
+
+
+unsafe fn create_main_window() -> Result<HWND> {
+    let class_name = w!("MyClass");
+    let window_name = w!("MyWindow");
+    let window_style = WS_OVERLAPPEDWINDOW;
+
+    let hinstance = GetModuleHandleW(Option::None).expect("Need hinstance");
+    let wc = WNDCLASSW {
+        hCursor: LoadCursorW(None, IDC_ARROW)?,
+        hInstance: hinstance.into(),
+        lpszClassName: class_name,
+        style: CS_HREDRAW | CS_VREDRAW,
+        lpfnWndProc: Some(wndproc),
+
+        ..Default::default()
+    };
+    let atom = RegisterClassW(&wc);
+    debug_assert!(atom != 0);
+
+    let hwnd = CreateWindowExW(
+        WINDOW_EX_STYLE::default(),
+        class_name,
+        window_name,
+        window_style,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        Option::None,
+        Option::None,
+        hinstance,
+        Option::None,
+    )?;
+    Ok(hwnd)
+}
+
+unsafe fn main_loop() {
+    let wmsgfiltermin = 0;
+    let wmsgfiltermax = 0;
+    let mut msg = MSG::default();
+    loop {
+        let message_available: bool = PeekMessageW(
+            &mut msg,
+            Option::None,
+            wmsgfiltermin,
+            wmsgfiltermax,
+            PM_REMOVE,
+        )
+        .into();
+        if message_available {
+            if msg.message == WM_QUIT {
+                break;
+            }
+            let _ = TranslateMessage(&msg);
+            DispatchMessageW(&msg);
+        }
+    }
+}
+
+
+extern "system" fn wndproc(window: HWND, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+    unsafe {
+        match message {
+            WM_PAINT => {
+                let _ = ValidateRect(window, None);
+                LRESULT(0)
+            }
+            WM_DESTROY => {
+                PostQuitMessage(0);
+                LRESULT(0)
+            }
+            _ => DefWindowProcW(window, message, wparam, lparam),
+        }
     }
 }
